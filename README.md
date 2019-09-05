210e21751ff0c70eef05997ad46bc083dad8fc6c
Martín Gaitán <gaitan@gmail.com>
2019-09-05 11:00:31 -0300

    funciona como app django

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ Esta versión incluye tambien los códigos de Chile desde [deis.cl](http://www.d
 pip install cie
 ```
 
-## Uso de la librería
+## Uso de la librería stand-alone
 
 ```python
 from cie.cie10 import CIECodes
@@ -91,3 +91,33 @@ for code in cie.search(txt='kaposi'):
 {'description': 'Sarcoma de Kaposi de múltiples órganos', 'source': 'icdcode.info', 'level': 5, 'depends_on': 'C46', 'multiple_descriptions': ['Sarcoma de Kaposi de múltiples órganos', 'Sarcoma de Kaposi', 'Tumores malignos de los tejidos mesoteliales y de los tejidos blandos', 'y afines', 'Tumores [neoplasias] malignos', 'Tumores [neoplasias]'], 'code': 'C468'}
 {'description': 'Sarcoma de Kaposi, de sitio no especificado', 'source': 'icdcode.info', 'level': 5, 'depends_on': 'C46', 'multiple_descriptions': ['Sarcoma de Kaposi, de sitio no especificado', 'Sarcoma de Kaposi', 'Tumores malignos de los tejidos mesoteliales y de los tejidos blandos', 'y afines', 'Tumores [neoplasias] malignos', 'Tumores [neoplasias]'], 'code': 'C469'}
 ```
+
+
+## Uso de la librería como aplicación Python
+
+Registra la aplicación
+
+
+```
+INSTALLED_APPS = (
+    ...
+    'cie',
+    ...
+)
+```
+
+Migra los datos para crear la tabla e importar los datos al modelo. 
+
+```
+python manage.py migrate cie
+```
+
+Uso:
+
+```python
+>>> from cie.models import CIE10Code
+>>> x511 = CIE10Code.objects.get(code='X511')
+```
+
+
+
--- setup.py
+++ setup.py
@@ -6,27 +6,26 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-     name='cie',
-     version='0.106',
-     license='MIT',
-     author="Andres Vazquez",
-     author_email="andres@data99.com.ar",
-     description="Lista de Códigos CIE10 en español",
-     long_description=long_description,
-     long_description_content_type="text/markdown",
-     url="https://github.com/cluster311/cie10",
-     install_requires=[
-        
-     ],
-     include_package_data=True,  # for ZIP file
-     packages=['cie'],  # setuptools.find_packages(),
-     
-     classifiers=[
-         'Programming Language :: Python :: 3',
-         'Programming Language :: Python :: 3.6',
-         'License :: OSI Approved :: MIT License',
-         'Operating System :: OS Independent',
-         'Intended Audience :: Developers', 
-     ],
-     python_requires='>=3.6',
- )
+    name='cie',
+    version='0.2',
+    license='MIT',
+    author="Andrés Vazquez",
+    author_email="andres@data99.com.ar",
+    description="Lista de Códigos CIE10 en español",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cluster311/cie10",
+    include_package_data=True,  # for ZIP file
+    packages=['cie'],  # setuptools.find_packages(),
+    extras_require={
+        'django':  ["Django>=1.11"],
+    },
+    classifiers=[
+     'Programming Language :: Python :: 3',
+     'Programming Language :: Python :: 3.6',
+     'License :: OSI Approved :: MIT License',
+     'Operating System :: OS Independent',
+     'Intended Audience :: Developers', 
+    ],
+    python_requires='>=3.6',
+)
